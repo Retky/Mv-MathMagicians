0129f5d35738496e5b6e4cf548b1bb4f0fbff2f6
Retky <jose.arredondo.a@outlook.com>
2022-04-19 11:49:17 -0500

    Link 'baseFiles' roots

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Calc from './components/Calculator';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Calc />);
