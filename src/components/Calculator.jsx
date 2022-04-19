e7225712e990ccda86942ac72030c65d98815ba4
Retky <jose.arredondo.a@outlook.com>
2022-04-19 15:27:58 -0500

    Modify 'calc' display style
    With colors

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,19 +4,24 @@ import './calculator.css';
 class Calc extends React.Component {
   constructor(props) {
     super(props);
-    this.btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+    this.btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
   }
 
   render() {
     return (
       <section className="calc">
-        <div className="calcScreen">Screen</div>
+        <div className="calcScreen">0</div>
         <div className="calcKeys">
           {this.btns.map((btn) => {
+            let genBtn;
             if (btn === '0') {
-              return (<button key={btn} className="calcBtn zero" type="button">{btn}</button>);
+              genBtn = (<button key={btn} className="calcBtn zero" type="button">{btn}</button>);
+            } else if (btn.match(/^['÷', 'x', '-', '+', '\-', '=']$/)) {
+              genBtn = (<button key={btn} className="calcBtn orangeBtn" type="button">{btn}</button>);
+            } else {
+              genBtn = (<button key={btn} className="calcBtn" type="button">{btn}</button>);
             }
-            return (<button key={btn} className="calcBtn" type="button">{btn}</button>);
+            return genBtn;
           })}
         </div>
       </section>
