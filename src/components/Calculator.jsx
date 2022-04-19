05f799e66c704c9c8109b604d4061b032bc19046
Retky <jose.arredondo.a@outlook.com>
2022-04-19 14:21:20 -0500

    Add 'calc' class to generate screen & keys

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import './calculator.css';
 
-const Calc = () => {
-  return (<div>Hello World</div>);
-};
+class Calc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  }
+
+  render() {
+    return (
+      <section className="calc">
+        <div className="calcScreen">Screen</div>
+        <div className="calcKeys">
+          {this.btns.map((btn) => {
+            if (btn === '0') {
+              return (<button key={btn} className="calcBtn zero" type="button">{btn}</button>);
+            }
+            return (<button key={btn} className="calcBtn" type="button">{btn}</button>);
+          })}
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Calc;
