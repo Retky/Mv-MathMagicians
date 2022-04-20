4e0da7bd9ffb43b01c542ca3c1f24f9dad0bfefb
Retky <jose.arredondo.a@outlook.com>
2022-04-20 16:45:33 -0500

    Add 'click' function to show operations

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
 class Calc extends React.Component {
   constructor(props) {
     super(props);
     this.btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-    this.object = {
-      total: null,
-      next: null,
-      operation: null,
+    this.state = {
+      object: {
+        total: null,
+        next: null,
+        operation: null,
+      },
     };
   }
 
-  display = () => {
-    if (this.object.total === null) {
-      return 0;
-    }
-    return this.object.total;
-  };
-
   clickHandler = (e) => {
-    console.log(e.target.name);
+    const { object } = this.state;
+    this.setState({ object: calculate(object, e.target.name) });
   }
 
   render() {
+    const { object } = this.state;
     return (
       <div className="calc">
-        <div className="calcScreen">{this.display()}</div>
+        <div className="calcScreen">{object.next ?? object.operation ?? object.total ?? 0}</div>
         <div className="calcKeys">
           {this.btns.map((btn) => {
             let genBtn;
