4dac1aa454fc2174554b6f82a97787ab5dc6b2aa
Retky <jose.arredondo.a@outlook.com>
2022-04-20 14:51:28 -0500

    Add 'clickHandler' to buttons

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,21 +5,37 @@ class Calc extends React.Component {
   constructor(props) {
     super(props);
     this.btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+    this.object = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  display = () => {
+    if (this.object.total === null) {
+      return 0;
+    }
+    return this.object.total;
+  };
+
+  clickHandler = (e) => {
+    console.log(e.target.name);
   }
 
   render() {
     return (
       <div className="calc">
-        <div className="calcScreen">0</div>
+        <div className="calcScreen">{this.display()}</div>
         <div className="calcKeys">
           {this.btns.map((btn) => {
             let genBtn;
             if (btn === '0') {
-              genBtn = (<button key={btn} className="calcBtn zero" type="button">{btn}</button>);
+              genBtn = (<button name={btn} key={btn} className="calcBtn zero" type="button" onClick={this.clickHandler}>{btn}</button>);
             } else if (btn.match(/^['÷', 'x', '-', '+', '\-', '=']$/)) {
-              genBtn = (<button key={btn} className="calcBtn orangeBtn" type="button">{btn}</button>);
+              genBtn = (<button name={btn} key={btn} className="calcBtn orangeBtn" type="button" onClick={this.clickHandler}>{btn}</button>);
             } else {
-              genBtn = (<button key={btn} className="calcBtn" type="button">{btn}</button>);
+              genBtn = (<button name={btn} key={btn} className="calcBtn" type="button" onClick={this.clickHandler}>{btn}</button>);
             }
             return genBtn;
           })}
