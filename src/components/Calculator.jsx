7c2a73a3ef8fa71007f866c57f1506769863a694
Retky <jose.arredondo.a@outlook.com>
2022-04-26 17:23:11 -0500

    Add 'pages' content

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -15,20 +15,23 @@ const Calc = () => {
   };
 
   return (
-    <div className="calc">
-      <div className="calcScreen">{object.next ?? object.operation ?? object.total ?? 0}</div>
-      <div className="calcKeys">
-        {btns.map((btn) => {
-          let genBtn;
-          if (btn === '0') {
-            genBtn = (<button name={btn} key={btn} className="calcBtn zero" type="button" onClick={clickHandler}>{btn}</button>);
-          } else if (btn.match(/^['÷', 'x', '-', '+', '\-', '=']$/)) {
-            genBtn = (<button name={btn} key={btn} className="calcBtn orangeBtn" type="button" onClick={clickHandler}>{btn}</button>);
-          } else {
-            genBtn = (<button name={btn} key={btn} className="calcBtn" type="button" onClick={clickHandler}>{btn}</button>);
-          }
-          return genBtn;
-        })}
+    <div className="mainCont">
+      <h2>Let&apos;s do some math!</h2>
+      <div className="calc">
+        <div className="calcScreen">{object.next ?? object.operation ?? object.total ?? 0}</div>
+        <div className="calcKeys">
+          {btns.map((btn) => {
+            let genBtn;
+            if (btn === '0') {
+              genBtn = (<button name={btn} key={btn} className="calcBtn zero" type="button" onClick={clickHandler}>{btn}</button>);
+            } else if (btn.match(/^['÷', 'x', '-', '+', '\-', '=']$/)) {
+              genBtn = (<button name={btn} key={btn} className="calcBtn orangeBtn" type="button" onClick={clickHandler}>{btn}</button>);
+            } else {
+              genBtn = (<button name={btn} key={btn} className="calcBtn" type="button" onClick={clickHandler}>{btn}</button>);
+            }
+            return genBtn;
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import './quote.css';
 
 const Quote = () => (
-  <div>Quote</div>
+  <p className="quote">
+    Mathematics is not about numbers, equations, computations, or algorithms; it is about
+    understanding.
+    <br />
+    <span> -William Paul Thurston</span>
+  </p>
 );
 
 export default Quote;
