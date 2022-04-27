17737904a0ec8112d14d6e74f992d7490a37c8ff
Teshome Kurabachew <dear.teshe@gmail.com>
2022-04-28 01:16:25 +0300

    Fix Linter Errors

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -2,21 +2,21 @@ import operate from './operate';
 
 describe('Operate', () => {
   test('SUM', () => {
-    expect(operate(2, 3, '+')).toBe('5')
+    expect(operate(2, 3, '+')).toBe('5');
   });
   test('REST', () => {
-    expect(operate(5, 3, '-')).toBe('2')
+    expect(operate(5, 3, '-')).toBe('2');
   });
   test('MULTIPLY', () => {
-    expect(operate(2, 3, 'x')).toBe('6')
+    expect(operate(2, 3, 'x')).toBe('6');
   });
   test('DIVIDE', () => {
-    expect(operate(8, 4, '÷')).toBe('2')
+    expect(operate(8, 4, '÷')).toBe('2');
   });
   test('ERROR', () => {
-    expect(operate(8, 0, '÷')).toBe("Can't divide by 0.")
+    expect(operate(8, 0, '÷')).toBe("Can't divide by 0.");
   });
   test('REMAINDER', () => {
-    expect(operate(8, 3, '%')).toBe("2")
+    expect(operate(8, 3, '%')).toBe('2');
   });
 });
